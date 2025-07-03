d70985752bb575f3d2aefb4a59f8bcf1838fc493
vikas012-code <vikasthapa115@gmail.com>
2025-07-03 12:28:59 +0530

    2nd commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-// import heroImg from '../assets/hero.jpg'; // use your own image
+import heroPage from "../assets/heroPage.webp"
+import heroPageSmalll from "../assets/HeropageImg.webp"
 
 const Hero = () => {
   return (
     <section className="z-10 relative bg-pink-50 overflow-hidden h-[90vh]">
-        <img className='-z-10 blur-[2px] object-cover w-[100vw] h-full absolute top-0 brightness-50 saturate-75' src="https://i1.larajewels.in/lara-images/page-images/lara-images_page-images_hero-banner-transformed.webp" alt="" />
-        <div className="z-20 max-w-7xl mx-auto px-6 py-20 flex flex-col md:flex-row items-center justify-center h-full ">
+        <img className='-z-10 blur-[2px] object-cover w-[100vw] h-full absolute top-0 brightness-50 saturate-75' src={heroPage} alt="" />
+        <div className="z-20 max-w-7xl mx-auto px-6 py-20 flex flex-col md:flex-row items-center justify-center h-full">
             <div className="md:w-1/2 space-y-6">
                 <h1 className="text-5xl font-bold text-emerald-600">Elegant. Timeless. Yours.</h1>
-                <p className="text-emerald-600 text-lg">
+                <p className="text-emerald-600 font-medium text-lg">
                     Discover handcrafted ornaments made with love and attention to detail.
                 </p>
             <button className="px-6 py-3 bg-pink-600 text-white rounded-full hover:bg-pink-700 transition">
@@ -16,7 +17,7 @@ const Hero = () => {
             </button>
             </div>
             <div className="md:w-1/2 mt-10 md:mt-0">
-                <img src={"https://ph.danielwellington.com/cdn/shop/files/Crystal_Jewellery-hero-1000x350.jpg?v=1740403168&width=1500"} alt="Hero" className="w-full rounded-xl shadow-lg" />
+                <img src={heroPageSmalll} alt="Hero" className="w-full rounded-xl shadow-lg" />
             </div>
         </div>
     </section>

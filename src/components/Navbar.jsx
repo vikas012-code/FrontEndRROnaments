3d71168e5bf923a1911f543b26f29b2866dcb504
vikas thapa <vikasthapa115@email.com>
2025-07-03 12:28:59 +0530

    2nd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50">
-      <div className="mx-auto px-6 py-2 flex items-center justify-between">
+    <nav className="bg-white shadow-lg sticky top-0 z-50 ">
+      <div className="mx-auto px-6 py-2 flex items-center justify-between shadow-sm">
         <a href='/' className="text-2xl font-bold text-gray-800"><img className='hover:scale-110 duration-200 w-26 ' src={logo} alt="" /></a>
 
       
@@ -30,7 +30,7 @@ const Navbar = () => {
 
      
       {isOpen && (
-        <ul className= {` md:hidden px-6 pb-4 space-y-3 text-sm font-medium text-gray-700 bg-white shadow-md`}>
+        <ul className= {`mt-1 pt-2 md:hidden px-6 pb-4 space-y-3 text-sm font-medium text-gray-700 bg-white shadow-md`}>
           <li><a href="#collections" onClick={() => setIsOpen(false)} className="block hover:text-pink-600">Collections</a></li>
           <li><a href="#testimonials" onClick={() => setIsOpen(false)} className="block hover:text-pink-600">Testimonials</a></li>
           <li><a href="#contact" onClick={() => setIsOpen(false)} className="block hover:text-pink-600">Contact</a></li>

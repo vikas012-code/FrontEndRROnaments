5ce74a63c3abe4cd499066fb1b144ab729bd7fb6
vikas012-code <vikasthapa115@gmail.com>
2025-07-03 12:15:13 +0530

    2nd commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className="z-10 relative bg-pink-50 overflow-hidden h-[90vh]">
-        <img className='-z-10 blur-[2px] object-cover w-[100vw] h-full absolute top-0' src="https://i1.larajewels.in/lara-images/page-images/lara-images_page-images_hero-banner-transformed.webp" alt="" />
+        <img className='-z-10 blur-[2px] object-cover w-[100vw] h-full absolute top-0 brightness-50 saturate-75' src="https://i1.larajewels.in/lara-images/page-images/lara-images_page-images_hero-banner-transformed.webp" alt="" />
         <div className="z-20 max-w-7xl mx-auto px-6 py-20 flex flex-col md:flex-row items-center justify-center h-full ">
             <div className="md:w-1/2 space-y-6">
-                <h1 className="text-5xl font-bold text-gray-800">Elegant. Timeless. Yours.</h1>
-                <p className="text-gray-600 text-lg">
+                <h1 className="text-5xl font-bold text-emerald-600">Elegant. Timeless. Yours.</h1>
+                <p className="text-emerald-600 text-lg">
                     Discover handcrafted ornaments made with love and attention to detail.
                 </p>
             <button className="px-6 py-3 bg-pink-600 text-white rounded-full hover:bg-pink-700 transition">
@@ -16,7 +16,7 @@ const Hero = () => {
             </button>
             </div>
             <div className="md:w-1/2 mt-10 md:mt-0">
-            <img src={"https://ph.danielwellington.com/cdn/shop/files/Crystal_Jewellery-hero-1000x350.jpg?v=1740403168&width=1500"} alt="Hero" className="w-full rounded-xl shadow-lg" />
+                <img src={"https://ph.danielwellington.com/cdn/shop/files/Crystal_Jewellery-hero-1000x350.jpg?v=1740403168&width=1500"} alt="Hero" className="w-full rounded-xl shadow-lg" />
             </div>
         </div>
     </section>

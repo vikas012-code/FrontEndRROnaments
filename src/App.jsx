47517f89a63de0de92bc0bfb941f04926883b4e4
vikas012-code <vikasthapa115@gmail.com>
2025-07-05 21:33:22 +0530

    experiment

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ import { UserContext } from './components/context';
 
 function App() {
 
-  const [collections ,setCollections] = useState([])
+  // const [collections ,setCollections] = useState([])
 
-  useEffect(()=>{
-    fetch("http://localhost:8000/products")
-    .then((res)=> res.json())
-    .then((res)=> setCollections([...res]))
-  },[])
+  // useEffect(()=>{
+  //   fetch("http://localhost:8000/products")
+  //   .then((res)=> res.json())
+  //   .then((res)=> setCollections([...res]))
+  // },[])
 
-  console.log(collections)
+  // console.log(collections)
 
-//   const collections = [
-//   { _id: 1,category:"necklace", name: 'Gold Necklace', price: '₹5,999', image: 'https://www.cuchara.ca/cdn/shop/products/DAINTYHEARTONSINGAPOREcopy.jpg?v=1663453620' },
-//   { _id: 2,category:"ring", name: 'Silver Ring', price: '₹2,499', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhxCFBtZSC00oLmjfCDIY1WTuW6M2lPs2DA&s' },
-//   { _id: 3,category:"bangles", name: 'Bangles', price: '₹3,299', image: 'https://d1311wbk6unapo.cloudfront.net/NushopCatalogue/tr:f-webp,w-600,fo-auto/64b7c91d3a2c4600127d44a9/cat_img/Latest_Gold_Bangles_Design_2025_Best_Gold_Plated_Bangles_M0RB5TFDN9_2024-12-31_1.jpg' },
-// ];
+  const collections = [
+  { _id: 1,category:"necklace", name: 'Gold Necklace', price: '₹5,999', image: 'https://www.cuchara.ca/cdn/shop/products/DAINTYHEARTONSINGAPOREcopy.jpg?v=1663453620' },
+  { _id: 2,category:"ring", name: 'Silver Ring', price: '₹2,499', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhxCFBtZSC00oLmjfCDIY1WTuW6M2lPs2DA&s' },
+  { _id: 3,category:"bangles", name: 'Bangles', price: '₹3,299', image: 'https://d1311wbk6unapo.cloudfront.net/NushopCatalogue/tr:f-webp,w-600,fo-auto/64b7c91d3a2c4600127d44a9/cat_img/Latest_Gold_Bangles_Design_2025_Best_Gold_Plated_Bangles_M0RB5TFDN9_2024-12-31_1.jpg' },
+];
   
 
   return (

b77326ee0e8a6e8d0d5a1e528542e207caf178c0
vikas thapa <vikasthapa115@email.com>
2025-07-11 18:56:45 +0530

    implemended server error components

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,6 +3,7 @@ import Card from './Card'
 import { useContext } from 'react'
 import { UserContext } from './context'
 import SkeletonLoading from './skelatonloading'
+import ServerError from './ServerError'
 
 function Category() {
     const {Id}=useParams()
@@ -31,20 +32,7 @@ function Category() {
           ))
 
           :
-
-            <div className="animate-fade-in-down bg-white border border-red-200 shadow-lg rounded-xl p-8 max-w-md text-center">
-              <div className="text-red-500 text-5xl mb-4">⚠️</div>
-              <h1 className="text-2xl font-semibold text-red-700 mb-2">500 | Server Error</h1>
-              <p className="text-red-600 mb-6">
-                Oops! Something went wrong on our end. Please try again later.
-              </p>
-              <button
-                onClick={() => window.location.reload()}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-200"
-              >
-                Retry
-              </button>
-            </div>
+           <ServerError/> 
           }
         </div>
       </div>

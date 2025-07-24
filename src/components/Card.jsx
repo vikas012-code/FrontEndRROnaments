ed22fdca55381beae6e62e7757cc518e0a305f15
vikas012-code <vikasthapa115@gmail.com>
2025-07-24 22:55:13 +0530

    developed product deleteand change quantity in admin panel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Card({item,price}) {
   
   return (
-    <div className=" bg-gray-50 mx-2 p-6 rounded-xl shadow active:shadow-xl hover:shadow-xl min-w-80 w-80 min-h-90 h-90 max-sm:min-w-50 max-sm:min-h-60 max-sm:w-50 max-sm:h-60">
+    <div className={`${!item.quantity && " opacity-60 saturate-50"} bg-gray-50 mx-2 p-6 rounded-xl shadow active:shadow-xl hover:shadow-xl min-w-80 w-80 min-h-90 h-90 max-sm:min-w-50 max-sm:min-h-60 max-sm:w-50 max-sm:h-60`}>
       <Link to={`${price?`/category/${item.category}`:`/product/${item._id}`}`} className='w-full h-full' >
         <img src={item.image} className="w-full h-[80%] object-cover rounded-lg " />
         <div className="mt-4 max-sm:mt-2 text-center">
--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -3,9 +3,9 @@ import Card from './Card';
 const Collections = () => {
 
   const collections=[
-  { _id: 1,category:"necklace", name: 'Gold Necklace', price: '₹5,999', image: 'https://www.cuchara.ca/cdn/shop/products/DAINTYHEARTONSINGAPOREcopy.jpg?v=1663453620' },
-  { _id: 2,category:"ring", name: 'Silver Ring', price: '₹2,499', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhxCFBtZSC00oLmjfCDIY1WTuW6M2lPs2DA&s' },
-  { _id: 3,category:"bangles", name: 'Bangles', price: '₹3,300', image: 'https://d1311wbk6unapo.cloudfront.net/NushopCatalogue/tr:f-webp,w-600,fo-auto/64b7c91d3a2c4600127d44a9/cat_img/Latest_Gold_Bangles_Design_2025_Best_Gold_Plated_Bangles_M0RB5TFDN9_2024-12-31_1.jpg' },
+  { _id: 1,category:"necklace", name: 'Gold Necklace', price: '₹5,999', image: 'https://www.cuchara.ca/cdn/shop/products/DAINTYHEARTONSINGAPOREcopy.jpg?v=1663453620' ,quantity:"true"},
+  { _id: 2,category:"ring", name: 'Silver Ring', price: '₹2,499', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhxCFBtZSC00oLmjfCDIY1WTuW6M2lPs2DA&s' ,quantity:"true"},
+  { _id: 3,category:"bangles", name: 'Bangles', price: '₹3,300', image: 'https://d1311wbk6unapo.cloudfront.net/NushopCatalogue/tr:f-webp,w-600,fo-auto/64b7c91d3a2c4600127d44a9/cat_img/Latest_Gold_Bangles_Design_2025_Best_Gold_Plated_Bangles_M0RB5TFDN9_2024-12-31_1.jpg' ,quantity:"true"},
   ]
 
   return (
